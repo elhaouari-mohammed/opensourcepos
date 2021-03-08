a5af65d3eb66ca14b2509cb3748bc5de0f898bba
Elhaouari Mohammmed <9967336+elhaouari-mohammed@users.noreply.github.com>
2021-03-08 10:06:14 +0100

    update bootstrap_tables french translations

--- application/language/fr/bootstrap_tables_lang.php
+++ application/language/fr/bootstrap_tables_lang.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
-$lang["tables_all"] = "Tous";
-$lang["tables_columns"] = "Colonnes";
-$lang["tables_hide_show_pagination"] = "Masquer/Afficher la pagination";
-$lang["tables_loading"] = "Chargement en cours, patientez, s?il vous pla?t ...";
-$lang["tables_page_from_to"] = "Affichage des lignes {0} à {1} sur {2} lignes au total";
-$lang["tables_refresh"] = "Rafra?chir";
-$lang["tables_rows_per_page"] = "{0} lignes par page";
-$lang["tables_toggle"] = "Alterner";
+$lang['tables_all'] = 'Tous';
+$lang['tables_columns'] = 'Colonnes';
+$lang['tables_hide_show_pagination'] = 'Cacher/Montrer la pagination';
+$lang['tables_loading'] = 'Chargement, veuillez patienter...';
+$lang['tables_page_from_to'] = 'Afficher {0} à {1} des {2} lignes';
+$lang['tables_refresh'] = 'Rafraîchir';
+$lang['tables_rows_per_page'] = '{0} lignes par page';
+$lang['tables_toggle'] = 'Toggle';
